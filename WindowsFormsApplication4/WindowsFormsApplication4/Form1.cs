10906a91ce0ad48961aa38745c7e1fdb41a96946
Vladimir <vsb95@mail.ru>
2018-01-28 21:09:42 +0600

    Изменил красный на розовый и исправил проблему 1го клика

--- WindowsFormsApplication4/WindowsFormsApplication4/Form1.cs
+++ WindowsFormsApplication4/WindowsFormsApplication4/Form1.cs
@@ -6,18 +6,20 @@ namespace WindowsFormsApplication4
 {
     public partial class Form1 : Form
     {
-        int clickCount;
+        static int _clickCount;
         public Form1()
         {
             InitializeComponent();
         }
         public void button1_Click(object sender, EventArgs e)
         {
-            label1.Text = clickCount++.ToString();
-            if (clickCount % 2 == 0)
+            // Обработчик события кнопки
+            _clickCount++;
+            label1.Text = _clickCount.ToString();
+            if (_clickCount % 2 == 0)
                 BackColor = Color.Yellow;
             else
-                BackColor = Color.Red;
+                BackColor = Color.HotPink;
 
         }
     }
