5cb387a4074fbcfb2dd3bd6d91af03443c19593b
Solenye <35895053+byn1kk@users.noreply.github.com>
2018-01-28 21:02:24 +0600

    инициализация репозитория

---
+++ WindowsFormsApplication4/WindowsFormsApplication4/Form1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication4
+{
+    public partial class Form1 : Form
+    {
+        int clickCount;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        public void button1_Click(object sender, EventArgs e)
+        {
+            label1.Text = clickCount++.ToString();
+            if (clickCount % 2 == 0)
+                BackColor = Color.Blue;
+            else
+                BackColor = Color.Red;
+
+        }
+    }
+}
